b5630f84c342ed3b96254a339324d5f9f1aa8167
Abhinav Tushar <abhinav@lepisma.xyz>
2020-11-25 00:52:45 +0530

    use flipped filters

--- lol/model.py
+++ lol/model.py
@@ -11,6 +11,42 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from lol.types import Audio
 
 
+def melspectrogram(
+    y=None,
+    sr=22050,
+    S=None,
+    n_fft=2048,
+    hop_length=512,
+    win_length=None,
+    window="hann",
+    center=True,
+    pad_mode="reflect",
+    power=2.0,
+    **kwargs,
+):
+    """
+    Patched melspectrogram function.
+    """
+
+    S, n_fft = librosa.core.spectrum._spectrogram(
+        y=y,
+        S=S,
+        n_fft=n_fft,
+        hop_length=hop_length,
+        power=power,
+        win_length=win_length,
+        window=window,
+        center=center,
+        pad_mode=pad_mode,
+    )
+
+    # Build a Mel filter
+    mel_basis = librosa.filters.mel(sr, n_fft, **kwargs)
+    mel_basis = np.flip(mel_basis, axis=1)
+
+    return np.dot(mel_basis, S)
+
+
 class Featurizer(BaseEstimator, TransformerMixin):
     """
     Default featurizer for audios. Uses inverted mel filters for MFCCs.
@@ -20,11 +56,10 @@ class Featurizer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: List[Audio], y=None):
-        # TODO: Invert filters
-
         n_mfcc = 20
         output = np.zeros((len(X), n_mfcc))
         for i, (y, sr) in enumerate(X):
-            output[i, :] = librosa.feature.mfcc(y, sr, n_mfcc=n_mfcc).mean(axis=1)
+            S = librosa.power_to_db(melspectrogram(y, sr))
+            output[i, :] = librosa.feature.mfcc(sr=sr, S=S, n_mfcc=n_mfcc).mean(axis=1)
 
         return output
